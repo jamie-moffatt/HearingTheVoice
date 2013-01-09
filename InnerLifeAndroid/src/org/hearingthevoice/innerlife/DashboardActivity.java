2457eaf91df09fa64db9e0a50ee4deb2bae5d57c
Jamie Bates <jamie_bates_8@live.co.uk>
2013-01-09 23:14:36 +0000

    Added question and schedule download to dashboard

--- InnerLifeAndroid/src/org/hearingthevoice/innerlife/DashboardActivity.java
+++ InnerLifeAndroid/src/org/hearingthevoice/innerlife/DashboardActivity.java
@@ -1,13 +1,18 @@
 package org.hearingthevoice.innerlife;
 
-import android.os.Bundle;
+import java.util.List;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashboardActivity extends Activity
 {
@@ -15,11 +20,20 @@ public class DashboardActivity extends Activity
 	private TextView txtQuestionsAvailable;
 	private ProgressBar progDownloadQuestions;
 	
+	private Runnable downloadQuestionsThread;
+	private Runnable downloadScheduleThread;
+	
+	private Activity activity;
+	private Context context;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_dashboard);
+		
+		context = getApplicationContext();
+		activity = this;
 		
 		btnAnswerTestQuestions = (Button)findViewById(R.id.btnAnswerTestQuestions);
 		txtQuestionsAvailable = (TextView)findViewById(R.id.txtQuestionsAvailable);
@@ -29,10 +43,9 @@ public class DashboardActivity extends Activity
 		progDownloadQuestions.setMax(100);
 		progDownloadQuestions.setProgress(0);
 		
-		try {Thread.sleep(2000);} catch (Exception e) {}
+		btnAnswerTestQuestions.setEnabled(false);
 		
-		progDownloadQuestions.setProgress(100);
-		txtQuestionsAvailable.setText("There are questions available.");
+		downloadSchedule();
 		
 		btnAnswerTestQuestions.setOnClickListener(new View.OnClickListener()
 		{
@@ -51,6 +64,90 @@ public class DashboardActivity extends Activity
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_dashboard, menu);
 		return true;
+	}
+	
+	public void downloadQuestions()
+	{
+		final Runnable callback = getQuestionCallback();
+		
+		downloadQuestionsThread = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Log.d("DOWNLOAD", "Downloading questions");
+					List<Section> sections = QuestionAPI.downloadQuestionXML(context, "questions.php");
+					if(sections != null) activity.runOnUiThread(callback);
+					else Toast.makeText(context, String.format("No questions downloaded"), Toast.LENGTH_SHORT).show();	
+				}
+				catch (Exception e) { e.printStackTrace(); }
+			}
+		};
+		
+		Thread thread = new Thread(null, downloadQuestionsThread, "DownloadQuestionsThread");
+
+		thread.start();
+		
+		Log.d("DOWNLOAD", "complete");
+	}
+
+	private Runnable getQuestionCallback()
+	{
+		Runnable callback = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				txtQuestionsAvailable.setText("Questions are available.");
+				progDownloadQuestions.setMax(100);
+				btnAnswerTestQuestions.setEnabled(true);
+			}
+		};
+
+		return callback;
+	}
+	
+	public void downloadSchedule()
+	{
+		final Runnable callback = getScheduleCallback();
+		
+		downloadScheduleThread = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				try
+				{
+					Log.d("DOWNLOAD", "Downloading schedule");
+					Schedule schedule = QuestionAPI.downloadScheduleXML(context, "schedule.php");
+					if(schedule != null) activity.runOnUiThread(callback);
+					else Toast.makeText(context, String.format("No schedule downloaded"), Toast.LENGTH_SHORT).show();
+				}
+				catch (Exception e) { e.printStackTrace(); }
+			}
+		};
+		
+		Thread thread = new Thread(null, downloadScheduleThread, "DownloadScheduleThread");
+
+		thread.start();
+		
+		activity.runOnUiThread(callback);
+	}
+
+	private Runnable getScheduleCallback()
+	{
+		Runnable callback = new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				downloadSchedule();
+			}
+		};
+
+		return callback;
 	}
 
 }
