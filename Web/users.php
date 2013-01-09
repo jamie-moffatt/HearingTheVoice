f4ecd0dd49625bca5aa5afee9db5ea8787a3f9a0
Matthew Bates <matthew117@hotmail.co.uk>
2013-01-09 20:49:04 +0000

    API can now produce user information.
    #NOTE: Would be a lot better to do this client side for security
    reasons.

--- Web/users.php
+++ Web/users.php
@@ -9,7 +9,46 @@ $post_data = file_get_contents('php://input');
 
 if ($_SERVER['REQUEST_METHOD'] === "GET")
 {
-	print("No User ID Given.");
+	if ($_GET['user'])
+	{
+		$user_id = $_GET['user'];
+
+		// read database configuration
+		$config = parse_ini_file("dbconfig.ini");
+
+		// make a mysql connection
+		$mysqli = new mysqli($config['host'], $config['user'], $config['password'], $config['database']);
+		if ($mysqli->connect_errno)
+		{
+			die("Failed to connect to MySQL: " . $mysqli->connect_error);
+		}
+
+		// list the sections
+		if ($result = $mysqli->query("SELECT * FROM `User` WHERE `userID` = '$user_id'"))
+		{
+			if ($row = $result->fetch_assoc())
+			{
+				// set content type to xml
+				header('Content-type: text/xml');
+				header('Charset: utf-8');
+				printf('<user id="%d" dob="%s" gender="%s" averageResponseTime="%s" />'."\n", $row['userID'], $row['DOB'], $row['gender'], getAverageResponseTime($mysqli, $user_id));
+			}
+			else
+			{
+				printf("User with id (%d) does not exist.\n", $user_id);
+			}
+			$result->free();
+		}	
+		else
+		{
+			print("Error retrieving data from MySQL: " . $mysqli->error . "\n");
+		}
+		$mysqli->close();
+	}
+	else
+	{
+		print("No User ID Given.");
+	}
 }
 elseif ($_SERVER['REQUEST_METHOD'] === "POST")
 {
@@ -52,6 +91,34 @@ else
 {
 	header('HTTP/1.1 405 Method Not Allowed', true, 405);
 	print("Method Not Allowed.");
+}
+
+function getNumberOfSubmissions($mysqli, $userID)
+{
+	// make be better to do this client side
+}
+
+function getAverageResponseTime($mysqli, $userID)
+{
+	if ($result = $mysqli->query("SELECT `notificationTime`, `submissionTime` FROM `Answer` WHERE `userID` = $userID GROUP BY `scheduleID`"))
+	{
+		$timeArray = array();
+		// calculate average time interval between notifications and submissions
+		while($row = $result->fetch_assoc())
+		{
+			$notificationDateTime = new DateTime($row['notificationTime']);
+			$submissionDateTime = new DateTime($row['submissionTime']);
+			$responseTimeInSeconds = $submissionDateTime->format('U') - $notificationDateTime->format('U');
+			$timeArray[] = $responseTimeInSeconds;
+		}
+		$result->free();
+		
+		return array_sum($timeArray) / count($timeArray);
+	}
+	else
+	{
+		return "UNKNOWN";
+	}
 }
 
 ?>
