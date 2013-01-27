eeeea2f03a62cdad40320b67d0798e1630ba454b
Matthew Bates <matthew117@hotmail.co.uk>
2013-01-27 19:00:38 +0000

    Improved graphics on the dashboard.
    Added icons to the dashboard. Still requires information updates.

--- InnerLifeAndroid/src/org/hearingthevoice/innerlife/DashboardActivity.java
+++ InnerLifeAndroid/src/org/hearingthevoice/innerlife/DashboardActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -17,6 +18,9 @@ import android.widget.Toast;
 public class DashboardActivity extends Activity
 {
 	private Button btnAnswerTestQuestions;
+	private TextView txtSamplesToday;
+	private TextView txtNumResponses;
+	private TextView txtResponseTime;
 	private TextView txtQuestionsAvailable;
 	private ProgressBar progDownloadQuestions;
 	
@@ -36,10 +40,14 @@ public class DashboardActivity extends Activity
 		activity = this;
 
 		btnAnswerTestQuestions = (Button) findViewById(R.id.btnAnswerTestQuestions);
+		txtSamplesToday = (TextView) findViewById(R.id.txtSamplesToday);
+		txtNumResponses = (TextView) findViewById(R.id.txtResponses);
+		txtResponseTime = (TextView) findViewById(R.id.txtResponseTime);
 		txtQuestionsAvailable = (TextView) findViewById(R.id.txtQuestionsAvailable);
 		progDownloadQuestions = (ProgressBar) findViewById(R.id.progDownloadQuestions);
-
+		
 		txtQuestionsAvailable.setText("Downloading Questions.");
+		txtQuestionsAvailable.setCompoundDrawablesWithIntrinsicBounds(R.drawable.action_download, 0, 0, 0);
 		progDownloadQuestions.setMax(100);
 		progDownloadQuestions.setProgress(0);
 
@@ -93,6 +101,9 @@ public class DashboardActivity extends Activity
 			Toast.makeText(context, "Schedule Downloaded.", Toast.LENGTH_LONG).show();
 			if (scheduleDownloaded && questionsDownloaded)
 			{
+				progDownloadQuestions.setProgress(100);
+				txtQuestionsAvailable.setText("New Questions Available.");
+				txtQuestionsAvailable.setCompoundDrawablesWithIntrinsicBounds(R.drawable.action_help, 0, 0, 0);
 				btnAnswerTestQuestions.setEnabled(true);
 			}
 		}
@@ -124,6 +135,9 @@ public class DashboardActivity extends Activity
 			Toast.makeText(context, "Questions Downloaded.", Toast.LENGTH_LONG).show();
 			if (scheduleDownloaded && questionsDownloaded)
 			{
+				progDownloadQuestions.setProgress(100);
+				txtQuestionsAvailable.setText("New Questions Available.");
+				txtQuestionsAvailable.setCompoundDrawablesWithIntrinsicBounds(R.drawable.action_help, 0, 0, 0);
 				btnAnswerTestQuestions.setEnabled(true);
 			}
 		}
