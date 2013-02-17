c80751bb1c83b121e57e3809cf7a60f1b3fbed37
Jamie Bates <jamie_bates_8@live.co.uk>
2013-02-17 23:35:57 +0000

    Updated summary page

--- InnerLifeAndroid/src/org/hearingthevoice/innerlife/ui/activity/SummaryActivity.java
+++ InnerLifeAndroid/src/org/hearingthevoice/innerlife/ui/activity/SummaryActivity.java
@@ -5,16 +5,34 @@ import org.hearingthevoice.innerlife.R;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class SummaryActivity extends Activity
 {
 	private Context context;
+	private ListView questionListView;
+	private Button btnConfirm;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_first_run_form);
+		setContentView(R.layout.summary_layout);
+		
+		questionListView = (ListView) findViewById(R.id.questionListView);
+		btnConfirm = (Button) findViewById(R.id.btnConfirmSubmission);
+		
+		btnConfirm.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				
+			}
+		});
 
 		context = this;
 	}
