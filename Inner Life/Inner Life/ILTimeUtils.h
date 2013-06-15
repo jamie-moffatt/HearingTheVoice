037807ab0decc80093e37971b694769d31f65535
Matthew Bates <matthew117@hotmail.co.uk>
2013-06-15 15:58:51 +0100

    Added a utility class for time functions.

---
+++ Inner Life/Inner Life/ILTimeUtils.h
@@ -0,0 +1,13 @@
+//
+//  ILTimeUtils.h
+//  Inner Life
+//
+//  Created by Matthew Bates on 15/06/2013.
+//  Copyright (c) 2013 Matthew Bates. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ILTimeUtils : NSObject
+
+@end
---
+++ Inner Life/Inner Life/ILTimeUtils.m
@@ -0,0 +1,13 @@
+//
+//  ILTimeUtils.m
+//  Inner Life
+//
+//  Created by Matthew Bates on 15/06/2013.
+//  Copyright (c) 2013 Matthew Bates. All rights reserved.
+//
+
+#import "ILTimeUtils.h"
+
+@implementation ILTimeUtils
+
+@end
