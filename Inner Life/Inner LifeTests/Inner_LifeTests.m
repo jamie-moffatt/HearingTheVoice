2d9b6eb940c878700f8ec2fbfcaf70428d84a8c5
Matthew Bates <matthew117@hotmail.co.uk>
2013-09-06 20:30:17 +0100

    Added some tests.

--- Inner Life/Inner LifeTests/Inner_LifeTests.m
+++ Inner Life/Inner LifeTests/Inner_LifeTests.m
@@ -7,26 +7,59 @@
 //
 
 #import "Inner_LifeTests.h"
+#import "ILAppManager.h"
+#import "NSDate-Utilities.h"
 
 @implementation Inner_LifeTests
 
 - (void)setUp
 {
     [super setUp];
-    
     // Set-up code here.
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
-    
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testNotifications
 {
-    STFail(@"Unit tests are not implemented yet in Inner LifeTests");
+    NSDate* now = [NSDate date];
+    [ILAppManager setStartDate:now];
+    [ILAppManager setupNotifications];
+    int am = [ILAppManager getAMNotificationTime];
+    int pm = [ILAppManager getPMNotificationTime];
+    
+    NSArray* notifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
+    
+    int n = [notifications count];
+    STAssertTrue(n == 28, @"There should be 28 notifications but there are %d", n);
+    
+    for (UILocalNotification* notification in notifications)
+    {
+        NSDate* notificationTime = notification.fireDate;
+        STAssertTrue([notificationTime isLaterThanDate:now], @"@% < @%", now, notificationTime);
+        
+        int sessionID = [[notification.userInfo objectForKey:@"SESSION_ID"] integerValue];
+        
+        NSDateComponents* dc = [[NSCalendar currentCalendar] components:(NSYearCalendarUnit | NSMonthCalendarUnit | NSDayCalendarUnit) fromDate:notificationTime];
+        NSDateComponents* startDC = [dc copy];
+        startDC.hour = (sessionID % 2) ? am : pm;
+        NSDateComponents* endDC = [dc copy];
+        endDC.hour = ((sessionID % 2) ? am : pm) + 3;
+        
+        NSDate* start = [[NSCalendar currentCalendar] dateFromComponents:startDC];
+        NSDate* end = [[NSCalendar currentCalendar] dateFromComponents:endDC];
+        
+        STAssertTrue(
+                     ([start isEarlierThanDate:notificationTime]
+                     && [end isLaterThanDate:notificationTime])
+                     || [start isEqualToDate:notificationTime]
+                     || [end isEqualToDate:notificationTime],
+                     @"%@ <= %@ <= %@", start, notificationTime, end);        
+    }
 }
 
 @end
