97bf52bd933774bf833503f5ee853df88a9196fe
Matthew Bates <matthew.bates.117@gmail.com>
2016-01-22 19:57:27 +0000

    Initial startup screen
    Added startup screen xib and barebones view controller.

---
+++ Inner Life/Inner Life/ILFirstTimeViewController.h
@@ -0,0 +1,19 @@
+//
+//  ILFirstTimeViewController.h
+//  Inner Life
+//
+//  Created by Matthew Bates on 22/01/2016.
+//  Copyright © 2016 Matthew Bates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ILFirstTimeViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UITextField *txtStudyCode;
+
+
+- (IBAction)onStartDemoTapped:(UIButton *)sender;
+- (IBAction)onStartFullApp:(UIButton *)sender;
+
+@end
---
+++ Inner Life/Inner Life/ILFirstTimeViewController.m
@@ -0,0 +1,42 @@
+//
+//  ILFirstTimeViewController.m
+//  Inner Life
+//
+//  Created by Matthew Bates on 22/01/2016.
+//  Copyright © 2016 Matthew Bates. All rights reserved.
+//
+
+#import "ILFirstTimeViewController.h"
+
+@interface ILFirstTimeViewController ()
+
+@end
+
+@implementation ILFirstTimeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)onStartDemoTapped:(id)sender {
+}
+
+- (IBAction)onStartFullApp:(UIButton *)sender {
+}
+@end
